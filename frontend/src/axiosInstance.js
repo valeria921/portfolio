1fab8b8ccc2a3f5787ae7548a5313c3cb283531a
valeria921 <valeria.kruk921@gmail.com>
2025-08-04 00:54:00 +0200

    url fix

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api';
+const baseURL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000/api/';
 
 const axiosInstance = axios.create({
 	baseURL: baseURL,
